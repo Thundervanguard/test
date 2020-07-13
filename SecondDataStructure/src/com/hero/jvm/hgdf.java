9da857387eb62b0e0b766e53a4f169389b2af243
arrow <liu27716854@163.com>
2020-07-14 07:58:07 +0800

    修改hgdf中的main方法2。。20020/7/14

--- SecondDataStructure/src/com/hero/jvm/hgdf.java
+++ SecondDataStructure/src/com/hero/jvm/hgdf.java
@@ -3,5 +3,6 @@ package com.hero.jvm;
 public class hgdf {
     public static void main(String[] args) {
         System.out.println("-------5555555555555555-------");
+        System.out.println("0000000000000000000000000");
     }
 }
